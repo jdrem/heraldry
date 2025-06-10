ab3b50bc81c74b4ebcedd9f802ddfd2e8053b554
Jeff Remillard <jdr@remgant.net>
2025-06-10 16:38:52 -0400

    Fixes for Ermine.

--- src/main/java/net/remgant/heraldry/tinctures/Ermine.java
+++ src/main/java/net/remgant/heraldry/tinctures/Ermine.java
@@ -20,16 +20,20 @@ public class Ermine extends Fur {
     public void fill(Graphics2D graphics, Area area) {
         ErmineSpot ermineSpot = new ErmineSpot(spotColor, null, 1.0);
         fieldColor.fill(graphics, area);
-        graphics.fill(area);
+        Rectangle2D ermineBounds = ermineSpot.getBounds();
+        double size = Math.max(ermineBounds.getWidth(), ermineBounds.getHeight()) * 0.67;
         Rectangle2D bounds = area.getBounds2D();
-        int columns = (int)(bounds.getWidth() / 30.0) + 1;
-        int rows = (int)(bounds.getHeight() / 30.0) + 1;
+        int columns = (int)(bounds.getWidth() / size) + 1;
+        int rows = (int)(bounds.getHeight() / size) + 1;
 
         for (int j=0; j<rows; j++) {
             for (int i=0; i<columns; i++) {
-                double x = i * 60.0 + (j % 2 == 1 ?  30.0 : 0.0);
-                double y = j * 30;
-                ermineSpot.draw(graphics, AffineTransform.getTranslateInstance(x, y));
+                double x = i * 2.0 * size + (j % 2 == 1 ?  size : 0.0);
+                double y = j * 2.0 * size;
+                AffineTransform at = new AffineTransform();
+                at.concatenate(AffineTransform.getScaleInstance(0.67, 0.67));
+                at.concatenate(AffineTransform.getTranslateInstance(x,y));
+                ermineSpot.draw(graphics, at);
             }
         }
     }
