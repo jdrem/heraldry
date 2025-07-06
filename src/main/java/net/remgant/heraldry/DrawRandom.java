56eb00193273f1dd7457b9ca8eadb2787a7ab04f
Jeff Remillard <jdr@remgant.net>
2025-07-06 10:20:16 -0400

    Make DrawRandom abstract. Create subclasses for ulti image and single page. #12

--- src/main/java/net/remgant/heraldry/DrawRandom.java
+++ src/main/java/net/remgant/heraldry/DrawRandom.java
@@ -76,6 +76,133 @@ public abstract class DrawRandom {
 
     abstract public void run(String[] args);
 
+    protected void drawShield(Builder builder, RandomGenerator random) {
+        double d = random.nextDouble();
+        Tincture fieldTincture = null;
+        Tincture chargeTincture = null;
+        Tincture ordinaryTincture = null;
+        Tincture t1 = null;
+        Tincture t2 = null;
+        Shield.LineOfDivision lineOfDivision = null;
+        if (d < 0.15) {
+            // split the field
+            t1 = pickAnyTincture(random);
+            t2 = pickAnyTinctureBut(random, Set.of(t1));
+            int ii = random.nextInt(Shield.LineOfDivision.values().length);
+            lineOfDivision = Shield.LineOfDivision.values()[ii];
+            builder.add(new Shield(t1, t2, lineOfDivision));
+        } else {
+            fieldTincture = pickAnyTincture(random);
+            builder.add(new Shield(fieldTincture));
+        }
+        // now decide what to do
+        d = random.nextDouble();
+        if (d < 0.60 && lineOfDivision != null &&
+                !(t1.isFur() && t2.isFur()) &&
+                !(t1.isMetal() && t2.isMetal()) &&
+                !(t1.isColor() && t2.isColor())) {
+            // Do a counter charge
+            MultiFunction<Tincture, Shield.Position, Double, Drawable> chargeFunc = charges.get(random.nextInt(charges.size()));
+            switch (lineOfDivision) {
+                case PER_PALE:
+                case PER_BEND:
+                case PER_BEND_SINISTER:
+                    builder.add(chargeFunc, t2, Shield.Position.FESS_DEXTER_MID, 2.0);
+                    builder.add(chargeFunc, t1, Shield.Position.FESS_SINISTER_MID, 2.0);
+                    break;
+                case PER_FESS:
+                    builder.add(chargeFunc, t2, Shield.Position.PALE_TOP_MID, 2.0);
+                    builder.add(chargeFunc, t1, Shield.Position.PALE_BOTTOM_MID, 2.0);
+                    break;
+            }
+        } else if (d < 0.40) {
+            // do an ordinary
+            Function<Tincture, Drawable> func = ordinaries.get(random.nextInt(ordinaries.size()));
+            if (fieldTincture == null && t1 != null && t2 != null) {
+                ordinaryTincture = pickAnyTinctureBut(random, Set.of(t1, t2));
+            } else if (fieldTincture != null && fieldTincture.isMetal())
+                ordinaryTincture = pickNonMetal(random);
+            else
+                ordinaryTincture = pickNonColor(random);
+            builder.add(func, ordinaryTincture);
+            // should we add charges to the ordinary?
+            d = random.nextDouble();
+            if (d < 0.35 && fieldTincture != null) {
+                // Add charges to ordinary
+                MultiFunction<Tincture, Shield.Position, Double, Drawable> chargeFunc = charges.get(random.nextInt(charges.size()));
+                String simpleName = func.apply(ordinaryTincture).getClass().getSimpleName();
+                d = random.nextDouble();
+                if (d < 0.5) {
+                    chargeTincture = fieldTincture;
+                } else {
+                    if (ordinaryTincture.isColor())
+                        chargeTincture = pickNonColor(random);
+                    else if (ordinaryTincture.isMetal())
+                        chargeTincture = pickNonMetal(random);
+                    else
+                        chargeTincture = pickAnyTinctureBut(random, Set.of(fieldTincture, ordinaryTincture));
+                }
+                Shield.ArrangementOfCharges arrangementOfCharges = Shield.ArrangementOfCharges.map.get(simpleName);
+                if (arrangementOfCharges != null)
+                    builder.add(chargeFunc, chargeTincture, arrangementOfCharges);
+            }
+        } else if (d < 0.55) {
+            // a single charge
+            MultiFunction<Tincture, Shield.Position, Double, Drawable> func = charges.get(random.nextInt(charges.size()));
+            if (fieldTincture == null) {
+                chargeTincture = pickAnyTinctureBut(random, Set.of(t1, t2));
+            } else if (fieldTincture.isMetal())
+                chargeTincture = pickNonMetal(random);
+            else
+                chargeTincture = pickNonColor(random);
+            builder.add(func, chargeTincture, Shield.Position.CENTER, 3.0);
+        } else {
+            // multiple charges.
+            MultiFunction<Tincture, Shield.Position, Double, Drawable> func = charges.get(random.nextInt(charges.size()));
+            if (fieldTincture == null) {
+                chargeTincture = pickAnyTinctureBut(random, Set.of(t1, t2));
+            } else if (fieldTincture.isMetal())
+                chargeTincture = pickNonMetal(random);
+            else
+                chargeTincture = pickNonColor(random);
+            int ii = random.nextInt(Shield.ArrangementOfCharges.allArrangements.length);
+
+            builder.add(func, chargeTincture, Shield.ArrangementOfCharges.allArrangements[ii]);
+        }
+        d = random.nextDouble();
+        if (d < 0.025) {
+            // add a border
+            Set<Tincture> ft = new HashSet<>();
+            if (t1 != null && t2 != null) {
+                ft.add(t1);
+                ft.add(t2);
+            } else {
+                ft.add(fieldTincture);
+            }
+            Tincture borderTincture = pickAnyTinctureBut(random, ft);
+            builder.add(new Border(borderTincture));
+        }
+        d = random.nextDouble();
+        if (d < 0.025) {
+            // add a border
+            Set<Tincture> ft = new HashSet<>();
+            if (t1 != null && t2 != null) {
+                ft.add(t1);
+                ft.add(t2);
+            } else {
+                ft.add(fieldTincture);
+            }
+            if (chargeTincture != null) {
+                ft.add(chargeTincture);
+            }
+            if (ordinaryTincture != null) {
+                ft.add(ordinaryTincture);
+            }
+            Tincture borderTincture = pickAnyTinctureBut(random, ft);
+            builder.add(new Label(borderTincture));
+        }
+    }
+
     public void addCharges(Tincture tincture, Builder builder, MultiFunction<Tincture, Shield.Position, Double, Drawable> func, Shield.Position[] positions) {
         for (Shield.Position position : positions) {
             builder.add(func, tincture, position, 1.0);
@@ -139,7 +266,7 @@ public abstract class DrawRandom {
         return t;
     }
 
-    private static Tincture pickColor(RandomGenerator random) {
+    protected static Tincture pickColor(RandomGenerator random) {
         double d = random.nextDouble();
         if (d < 0.30)
             return Tincture.GULES;
